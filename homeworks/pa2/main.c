7b6fe9b11caa8c28669cac0bfcaf39534a7ae573
pintchom <124115015+pintchom@users.noreply.github.com>
2023-10-11 21:11:37 -0400

    minor print changes

--- homeworks/pa2/main.c
+++ homeworks/pa2/main.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[]) {
         Converter converter;        
         FP_TYPE user_input;
         printf("Please enter a floating-point number or q to quit.\n");
+        printf("> ");
      
-        while (scanf(FP_TYPE_FORMAT "> ", &user_input)) {
+        while (scanf(FP_TYPE_FORMAT, &user_input)) {
             
             printf(FP_TYPE_FORMAT "\n", user_input); 
             converter.float_rep = user_input;
             print_components(converter); 
-
-            //printf(FP_TYPE_FORMAT "\n", power_of_2(3)) 
-            //print_components(user_input);
+            
+            printf("> ");
         } 
     }
 
