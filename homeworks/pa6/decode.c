92243186a8c554109fee37ee028e074554f5206b
Max Pintchouk <maxpintchouk4321@gmail.com>
2023-11-15 15:43:45 -0500

    basic encode decode template code

--- homeworks/pa6/decode.c
+++ homeworks/pa6/decode.c
@@ -1,15 +1,47 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char decode(const int count, const int bits[]) {
-
+    unsigned char output = 0;
+    for (int i = 0; i < count; i++) {
+        int originalBit = (9 - bits[i] + 8 - count) % 8;
+        output |= (1 << originalBit);
+    }
+    return output;
 }
 
-void read_and_decode_file(const char * file_name) {
+void read_and_decode_file(const char *file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (!file) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
 
+    int count;
+    while (fscanf(file, "%d", &count) == 1) {
+        int bits[count];
+        for (int i = 0; i < count; i++) {
+            if (fscanf(file, "%d", &bits[i]) != 1) {
+                fprintf(stderr, "Error reading encoded data.\n");
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        char decodedChar = decode(count, bits);
+        printf("%c", decodedChar);
+    }
+
+    fclose(file);
 }
 
-int main(int argc, char * argv[]) {
-    
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <encoded_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    read_and_decode_file(argv[1]);
+    return EXIT_SUCCESS;
 }
 
--- homeworks/pa6/encode.c
+++ homeworks/pa6/encode.c
@@ -1,12 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int encode(unsigned char input, int * indices) {
+#define MAX_BITS 8
 
+int getSetBits(unsigned char ch, int *bits) {
+    int count = 0;
+    for (int i = 0; i < 8; ++i) {
+        if (ch & (1 << i)) {
+            bits[count++] = i;
+        }
+    }
+    return count;
 }
 
-void read_and_encode_file(char * in_file, char * out_file) {
-    
+int encode(unsigned char input, int *indices) {
+    int bits[8];
+    int count = getSetBits(input, bits);
+
+    for (int i = 0; i < count; ++i) {
+        int rotated = (bits[i] + count) % 8;
+        indices[i] = 9 - rotated;
+    }
+
+    return count;
+}
+
+void read_and_encode_file(char *in_file, char *out_file) {
+    FILE *fin, *fout;
+    int ch;  
+    int indices[MAX_BITS];
+    int count, i;
+
+    fin = fopen(in_file, "r");
+    if (!fin) {
+        perror("Error opening input file");
+        exit(EXIT_FAILURE);
+    }
+
+    fout = fopen(out_file, "w");
+    if (!fout) {
+        perror("Error opening output file");
+        fclose(fin);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = fgetc(fin)) != EOF) {
+        count = encode((unsigned char)ch, indices);
+
+        fprintf(fout, "%d", count);
+        for (i = 0; i < count; i++) {
+            fprintf(fout, "%d", indices[i]);
+        }
+    }
+
+    fclose(fin);
+    fclose(fout);
 }
 
 int main(int argc, char * argv[]) {
@@ -24,3 +72,4 @@ int main(int argc, char * argv[]) {
     return EXIT_SUCCESS;
     
 }
+
